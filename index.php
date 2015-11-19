062a41611037c7a5790edd2836b8bd9d4c9a7e1a
Chester96 <luigihack32@gmail.com>
2015-11-19 22:29:21 +0100

    basics

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+//header
 include("./template_pagine/header.php");
+?>
+
+<div id="contenitore" class="contenitore">
+    
+</div>
+<script type="text/javascript">
+    $("document").ready(function()
+    {
+        alert("ciao");
+    });
+</script>
+<?php
+//footer
 include("./template_pagine/footer.php");
 ?>
--- template_pagine/header.php
+++ template_pagine/header.php
@@ -2,5 +2,8 @@
     <head>
         <title>NOMEBAND</title>
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <script type="text/javascript" src="/js/jquery.js"></script>
+        <link href="http://meteora.dev/css/style.css" rel="stylesheet" type="text/css">
     </head>
     <body>
